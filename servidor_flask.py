878ca829e9fda0597015be48fed16c4a2f0f35bc
winbrasil <147886395+winbrasil@users.noreply.github.com>
2025-10-16 18:40:09 -0300

    Add files via upload

--- servidor_flask.py
+++ servidor_flask.py
@@ -3,6 +3,15 @@ from flask import Flask, request, jsonify
 
 app =  Flask(__name__)
 
+@app.post("/auth/verify")
+def verify():
+    data = request.get_json(silent=True) or {}
+    token = data.get("id_token")
+    
+    if not token:
+        return jsonify({"ok": False, "error": "id_token ausente!"}), 400
+
+
 @app.route("/")
 def raiz():
     return "online"
@@ -11,7 +20,7 @@ def raiz():
 def servidor():
     if not request.is_json:
         return jsonify(error="Envie JSON no corpo da requesição."), 400
-    data = request.get_json(silent=True)or {}
+    data = request.get_json(silent=True) or {}
     
     msg = data.get("msg", "Sem mensagem")
     
